36f0dc7b94c7299450545746382a07eb0bdb54a8
https://github.com/MiriamSP/GIT.git <miriam.sanz.pascua@alumnos.upm.es>
2016-03-11 19:01:54 +0100

    primeros cambios en daos

--- src/main/java/business/controllers/TokenController.java
+++ src/main/java/business/controllers/TokenController.java
@@ -1,5 +1,8 @@
 package business.controllers;
 
+import java.util.Calendar;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +30,16 @@ public class TokenController {
     public void setUserDao(UserDao userDao) {
         this.userDao = userDao;
     }
+    
+    public void deleteExpiredToken(){
+        List<Token> token = tokenDao.findAllTokens();
+        //token.isTokenExpired(Calendar.getInstance().getTimeInMillis());
+        
+        
+    }
 
     public String login(String username) {
+         
         User user = userDao.findByUsernameOrEmail(username);
         assert user != null;
         Token token = new Token(user);
--- src/main/java/data/daos/TokenDao.java
+++ src/main/java/data/daos/TokenDao.java
@@ -1,6 +1,9 @@
 package data.daos;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import data.entities.Token;
 import data.entities.User;
@@ -8,4 +11,8 @@ import data.entities.User;
 public interface TokenDao extends JpaRepository<Token, Integer> {
 
     Token findByUser(User user);
+    
+    @Query("select * from token")
+    List<Token>findAllTokens();
+    
 }
