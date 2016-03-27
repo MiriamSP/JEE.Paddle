47578a32118f4cd52b1a0f001face5ff98e35aa1
msanzpascua <msanzpascua@aeport1067.acciona.int>
2016-03-27 17:27:32 +0200

    cambio create con Service

--- src/main/java/web/Presenter.java
+++ src/main/java/web/Presenter.java
@@ -27,6 +27,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 import business.controllers.CourtController;
 import business.wrapper.CourtState;
+import data.entities.Court;
+import data.services.CourtService;
 
 @Controller
 @Scope(WebApplicationContext.SCOPE_SESSION)
@@ -35,9 +37,12 @@ public class Presenter {
     
     @Autowired
     private CourtController courtController;
+    
+    @Autowired
+    private CourtService courtService ;
 
     private static final List<String> THEMES = Arrays.asList("jsp", "bootstrap", "thymeleaf");
-
+    
     @Autowired
     private ServletContext servletContext;
 
@@ -45,6 +50,7 @@ public class Presenter {
     private String theme = THEMES.get(0);
 
     public Presenter() {
+        courtService = new CourtService();      
     }
 
     // Se ejecuta siempre y antes. Añade un atributo al Model
@@ -55,6 +61,7 @@ public class Presenter {
 
     @RequestMapping("/home")
     public String home(Model model) {
+        
         model.addAttribute("themes", THEMES);
         //La vista resultante no lleva extensión (.jsp) configurado en WebConfig.java
         return theme + "/home";
@@ -66,14 +73,6 @@ public class Presenter {
         return new ModelAndView(theme + "/home", "themes", THEMES);
     }
     
-    @RequestMapping("/create-court")
-    public ModelAndView createCourtmsp(@RequestParam String theme) {
-        this.theme = theme;
-        return new ModelAndView(theme + "/home", "themes", THEMES);
-    }
-    
-   
-
     @RequestMapping(value = "/greeting")
     public String greeting(@CookieValue("JSESSIONID") Cookie cookie, HttpServletRequest request, Model model) {
         model.addAttribute("stringList", Arrays.asList("uno", "dos", "tres"));
@@ -81,20 +80,71 @@ public class Presenter {
         model.addAttribute("ip", request.getRemoteAddr());
         return theme + "/greeting";
     }
-
-   
+    
     @RequestMapping("/court-list")
+    public ModelAndView listCourts(Model model) {
+       // ModelAndView modelAndView = new ModelAndView(theme + "/userList");
+        ModelAndView modelAndView = new ModelAndView("jsp/courtList");
+        modelAndView.addObject("courtList", courtService.findAll());
+        return modelAndView;
+    }
+
+   /*
+    @RequestMapping(value = "/court-list", method = RequestMethod.GET)
     public String listCourts(Model model){
-        model.addAttribute("courtsList", courtController.showCourts());
+        //model.addAttribute("courtsList", courtController.showCourts());
+        System.out.println("PRESENTER: " + courtService.findAll());
+        model.addAttribute("courtsList", courtService.findAll());
         return "jsp/courtList";
     }
-    
+    */
     @RequestMapping(value ="/create-court", method = RequestMethod.GET)
     public String createCourt(Model model){
-        model.addAttribute("court", new CourtState(courtController.nextCourtId(), true));
+        //model.addAttribute("court", new CourtState(courtController.nextCourtId(), true));
+        //System.out.println("PRESENTER: " + courtService.findAll());
+        //System.out.println("PRESENTER: " + courtService.generateId());
+        Court court = new Court(courtService.generateId());
+        System.out.println("PRESENTER - new court: " + court.getId());
+        model.addAttribute("court", court);
+        //model.addAttribute("court", new Court(courtService.generateId()));
         return "jsp/createCourt";
+        // return theme + "/createUser";
     }
+        
+   
+    @RequestMapping(value = "/create-court", method = RequestMethod.POST)
+    public String createCourtSubmit(@Valid Court court, BindingResult bindingResult, Model model) {
+        System.out.println("PRESENTER - bindingResult : " + bindingResult.toString());
+        System.out.println("PRESENTER - model  : " + model.toString());
 
+        if (!bindingResult.hasErrors()) {
+            System.out.println("PRESENTER - save: " + court.getId());
+
+            if (courtService.save(court)) {
+                model.addAttribute("id", court.getId());
+                return //theme + 
+                        "jsp/registrationSuccess";
+            } else {
+                bindingResult.rejectValue("id", "error.court", "Court ya existente");
+            }
+        }
+        return //theme + 
+                "jsp/createCourt";
+    }
+    
+    
+    
+    /*
+    
+    @RequestMapping("/create-court")
+    public ModelAndView createCourtmsp(@RequestParam String theme) {
+        this.theme = theme;
+        return new ModelAndView(theme + "/home", "themes", THEMES);
+    }
+    */
+
+    
+    
    
 
 }
